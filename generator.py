d030729c3c1402fa8839f4253f633d9ad7d1edf1
will-jac <j.williams@ou.edu>
2021-05-01 13:49:50 -0500

    Fixed pseudo labelling accuracy problem during training

--- generator.py
+++ generator.py
@@ -2,14 +2,16 @@ import random
 import numpy as np
 
 # I'd really love for this to be deterministic, but it won't be, and really, that's okay
-def training_generator(data, batch_size=10):
+def training_generator(data, batch_size=10, min_labeled_per_epoch=None):
     # x_index = u_index = 0
-    labeled_to_total_ratio = data.X.shape[0] / (data.X.shape[0] + data.U.shape[0])
-
-    x_size = int(round(batch_size*(labeled_to_total_ratio)))
-    u_size = int(round(batch_size*(1-labeled_to_total_ratio)))
-    # x_end = X.shape[0] - 1
-    # u_end = U.shape[0] - 1
+    if min_labeled_per_epoch:
+        labeled_to_total_ratio = data.X.shape[0] / (data.X.shape[0] + data.U.shape[0])
+    
+        x_size = int(round(batch_size*(labeled_to_total_ratio)))
+        u_size = int(round(batch_size*(1-labeled_to_total_ratio)))
+    else:
+        x_size = min_labeled_per_epoch
+        u_size = batch_size - min_labeled_per_epoch
 
     print('creating generator..', 'batch', batch_size, 'ratio', labeled_to_total_ratio, 'sizes', x_size, u_size)
     while True:
@@ -25,15 +27,14 @@ def pseudo_label_generator(data, batch_size=10, unlabeled=-1):
         label_shape = tuple(data.U.shape[0])
     else:
         label_shape = (data.U.shape[0], data.y.shape[1])
+    # prelabel the psuedo labels
     pseudo_labels = unlabeled * np.ones(label_shape, dtype=float)
-        
-    X = np.append(data.X, data.U, axis=0)
-    y = np.append(data.y, pseudo_labels, axis=0)
 
-    print('creating generator..', 'batch', batch_size, 'ratio', labeled_to_total_ratio, 'sizes', x_size, u_size)
+    print('creating generator..', 'batch', batch_size)
     while True:
         # Randomly select a set of example indices
         Xi = np.random.randint(0, X.shape[0], batch_size)
 
         # The generator will produce a pair of return values: one for inputs and one for outputs
+        # print(Xi)
         yield (X[Xi], y[Xi])
--- pseudo_labels.py
+++ pseudo_labels.py
@@ -57,7 +57,7 @@ class PseudoLabels():
         self.callbacks = [
             keras.callbacks.EarlyStopping(
                 patience=patience, restore_best_weights=True, min_delta=min_delta,
-                monitor='categorical_accuracy'
+                monitor='val_pl_categorical_accuracy'
             ),
             # alpha scheduler
             keras.callbacks.LambdaCallback(
@@ -93,13 +93,13 @@ class PseudoLabels():
     def alpha_schedule(self, epoch_num):
         if epoch_num < self.T1:
             # print('alpha set to 0.0')
-            return 0.0
-        if epoch_num < self.T2:
+            self.alpha = 0.0
+        elif epoch_num < self.T2:
             # print('alpha set to', (epoch_num - self.T1) / (self.T2 - self.T1) * self.af)
-            return (epoch_num - self.T1) / (self.T2 - self.T1) * self.af
+            self.alpha = (epoch_num - self.T1) / (self.T2 - self.T1) * self.af
         else:
             # print('alpha set to', self.af)
-            return self.af
+            self.alpha = self.af
 
     def pl_loss(self, y_true, y_pred):
         # tf.print('loss for:', y_true.shape, y_pred.shape, y_true[0])
@@ -135,14 +135,33 @@ class PseudoLabels():
         # Set coefficient for each sample based on whether labeled or unlabeled
         index = K.all(index, axis=1)
         coef_arr = tf.where(index, self.alpha, 1.0)
+
+        # tf.print(self.alpha)
         # tf.print('coef_arr', coef_arr.shape, coef_arr)
 
         # compute the loss
         loss = keras.losses.categorical_crossentropy(y_pl, y_pred)
         # tf.print('loss', loss.shape, loss)
         # loss = tf.nn.softmax_cross_entropy_with_logits(y_true, y_pred)
+        # tf.print(tf.reduce_sum(coef_arr * loss))
+        return tf.reduce_sum(coef_arr * loss)
 
-        return coef_arr * loss
+    def pl_loss_test(self, y_true, y_pred):
+        pl = K.one_hot(K.argmax(y_pred), self.out_size)
+        pl = tf.cast(pl, y_true.dtype)
+
+        index = y_true == self.unlabeled
+
+        y_pl = tf.where(index, pl, y_true)
+
+        index = K.all(index, axis=1)
+        # coef_arr = tf.where(index, self.alpha, 1.0)
+
+        loss = keras.losses.categorical_crossentropy(y_pl, y_pred)
+        labeled_loss = tf.reduce_sum(tf.where(index, 0, loss))
+        unlabeled_loss = tf.reduce_sum(tf.where(index, loss, 0))
+
+        return labeled_loss + self.alpha * unlabeled_loss
 
     def prelabel_data(self, data):
         if data.y.shape[1] is None:
@@ -150,23 +169,31 @@ class PseudoLabels():
         else:
             label_shape = (data.U.shape[0], data.y.shape[1])
         pseudo_labels = self.unlabeled * np.ones(label_shape, dtype=float)
+        
+        assert(data.X.shape[0] == data.y.shape[0])
+        assert(data.X.shape[1] == data.U.shape[1])
+        assert(data.y.shape[1] == pseudo_labels.shape[1])
 
         X = np.append(data.X, data.U, axis=0)
         y = np.append(data.y, pseudo_labels, axis=0)
 
-        
+        return Data(X, y, None)
 
-        return Data(
-            np.append(data.X, data.U, axis=0),
-            np.append(data.y, pseudo_labels, axis=0),
-            None
-        )
+    def pl_categorical_accuracy(self, y_true, y_pred):
+        index = y_true == self.unlabeled
+
+        index = K.all(index, axis=1)
+
+        acc = keras.metrics.categorical_accuracy(y_true, y_pred)
+        # tf.print('acc', acc.shape, 'index', index.shape)
+        # return tf.reduce_mean(acc, axis=-1)
+        return tf.reduce_sum(tf.where(index, 0.0, acc), axis=-1) / tf.cast(tf.reduce_sum(tf.where(index, 0, 1), axis=-1), tf.float32)
 
     def fit(self, train_data, validation_data):
         print('train data:', train_data.X.shape, train_data.y.shape, train_data.U.shape)
         u = train_data.U.shape[0]
 
-        # train_data = self.prelabel_data(train_data)
+        train_data = self.prelabel_data(train_data)
 
         # print('prelabeled data:', train_data.X.shape, train_data.y.shape)
 
@@ -200,19 +227,20 @@ class PseudoLabels():
         self.model.summary()
 
         # compile the model
+        # tf.keras.Model.compile(self)
         self.model.compile(
             loss=self.pl_loss, 
             optimizer=tf.keras.optimizers.Adam(lr=self.lrate),
-            metrics=self.metrics
+            metrics=self.pl_categorical_accuracy
         )
 
         # now, do the training (pseudo-labels & conditional cross-entropy)
 
         print('training on:', train_data.X.shape, train_data.y.shape)
-
         history = self.model.fit(
-            x=pseudo_label_generator(train_data, self.batch_size, self.unlabeled),
-            validation_data=(np.array(validation_data.X), np.array(validation_data.y)),
+            x=train_data.X, #pseudo_label_generator(train_data, self.batch_size, self.unlabeled),
+            y=train_data.y,
+            validation_data=(validation_data.X, validation_data.y),
             callbacks=self.callbacks,
             epochs=self.epochs,
             steps_per_epoch=self.steps_per_epoch
